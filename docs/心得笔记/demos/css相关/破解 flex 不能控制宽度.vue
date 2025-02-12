1246cfd5b33384a72106d7b3af5ee3e14d1e6b5a
cenyp <2538966470@qq.com>
2025-02-12 19:55:49 +0800

    feat 破解 flex 不能控制宽度

---
+++ docs/心得笔记/demos/css相关/破解 flex 不能控制宽度.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div>设置 width: 20%</div>
+    <div class="container">
+      <div class="item">1</div>
+      <div class="item" style="width: 20%">2</div>
+      <div class="item">3</div>
+    </div>
+    <div>设置 min-width: 20%;max-width: 20%;</div>
+    <div class="container">
+      <div class="item">1</div>
+      <div class="item" style="min-width: 20%;max-width: 20%;">2</div>
+      <div class="item">3</div>
+    </div>
+  </div>
+</template>
+<script setup></script>
+<style scoped>
+.container {
+  display: flex;
+  margin-bottom: 10px;
+  gap: 6px;
+}
+.item {
+  flex: 1;
+  border: 1px solid #000;
+  text-align: center;
+}
+</style>
